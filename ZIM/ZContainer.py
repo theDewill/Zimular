f365005f8f80d55d12d51efc28bf1b5967e56c46
Nomin Sendinu <soloriderns@gmail.com>
2023-12-26 10:05:57 +0530

    Add: load and consume procedures

--- ZIM/ZContainer.py
+++ ZIM/ZContainer.py
@@ -1,20 +1,69 @@
 from simpy import Container
  
-class ContainerGenerator():
+'''
+1) can use ZContainerGenerator to create container pools and manage them 
+2) use ZContainer to create a single container and manage it
+''' 
+class ZContainerGenerator():
     def __init__(self,env,containers):
         self.CPool = {}
         self.env = env
-        #thsi shows how the containers are selected for populating and consuming items
-        self.procedure = 
-
-    def createContainers(self,containerName,count):
-        self.CPool.setdefault(containerName)
-        for i in range(count):
-            #supposing inital value is 0, if not ficking give it as a parameter
-            self.CPool[containerName].append(ZContainer(self.env,100)) 
+        self.procedure = 1 
+    '''
+        thsi shows how the containers are selected for populating and consuming items
+        1 (default)- when populating the highest level is filled first then go for rest, consumed from lowest level container first
+        (always there will be a full container mosly )
+        2 - when populating the lowest level is filled first then go for rest, consumed from highest level container first
+    '''
         
-    def requestContainer(self,containerName):
-        for container in self.CPool[containerName]:
+    '''
+    must pass an array including container configs for each container in count[[capacity,initialValue],etc..]
+    '''
+    def createContainers(self,containerName,count,configs):
+        self.CPool.setdefault(containerName)
+        for c in range(count):
+            #supposing inital value is 0, if not  give it as a parameter
+            self.CPool[containerName].append(ZContainer(self.env,containerName,c,configs[c][0],configs[c][1])) 
+
+    def load(self,containerName,amount):
+        if self.procedure == 1:
+            chosenContainer = None
+            highestContainerLevel = 0
+            for container in self.CPool[containerName]:
+                if container.container.level > highestContainerLevel:
+                    chosenContainer = container
+                    highestContainerLevel = container.container.level
+            chosenContainer.load(amount)
+        else:
+            chosenContainer = None
+            lowestContainerLevel = self.CPool[containerName][0].container.level
+            for container in self.CPool[containerName]:
+                if container.container.level < lowestContainerLevel:
+                    chosenContainer = container
+                    lowestContainerLevel = container.container.level
+            chosenContainer.load(amount)
+        
+    def consume(self,containerName,amount):
+        if self.procedure == 1:
+            chosenContainer = None
+            lowestContainerLevel = self.CPool[containerName][0].container.level
+            for container in self.CPool[containerName]:
+                if container.container.level < lowestContainerLevel:
+                    chosenContainer = container
+                    lowestContainerLevel = container.container.level
+            chosenContainer.consume(amount)
+        else:
+            chosenContainer = None
+            highestContainerLevel = 0
+            for container in self.CPool[containerName]:
+                if container.container.level > highestContainerLevel:
+                    chosenContainer = container
+                    highestContainerLevel = container.container.level
+            chosenContainer.consume(amount)
+        
+    
+            
+
 
 
 
@@ -44,7 +93,7 @@ class ZContainer():
     def Empty(self):
         self.container.get(self.container.level)
 
-    def populate(self,amount):
+    def load(self,amount):
         self.container.put(amount)
     
     def consume(self,amount):
