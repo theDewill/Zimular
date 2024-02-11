4fed42acabf84ecc99ddc330fc2fde522981dd6f
navindu-sachintha <navindu.social@gmail.com>
2024-02-11 18:56:39 +0530

    Feature: add logout route

--- GUI/src/app/profile/page.tsx
+++ GUI/src/app/profile/page.tsx
@@ -1,9 +1,33 @@
+"use client"
+import axios from "axios";
+import Link from "next/link";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation"; 
+
 export default function ProfilePage() {
+
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get('api/users/logout');
+            toast.success("Logout Success");
+            router.push('/login');
+        } catch (error:any) {
+            console.log(error.message);
+            toast.error (error.message)
+        }
+    }
     return (
         <div className=" flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <hr />
+
+            <button onClick={logout}
+                    className="bg-blue-500 mt-4 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Logout
+                    </button>
         </div>
     )
 }
