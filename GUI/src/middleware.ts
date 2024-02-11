15998a1963a9f839adfcb803da4572e43db3b716
navindu-sachintha <navindu.social@gmail.com>
2024-02-11 22:26:17 +0530

    Feature: Add path restrictions

---
+++ GUI/src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
+
+    const isPublicPath = path === '/login' || path === '/signup' || path === '/profile/:path*';
+
+    const token = request.cookies.get('token')?.value || '';
+    
+    if (isPublicPath && token){
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+
+    if (!isPublicPath && !token){
+        return NextResponse.redirect(new URL('/login', request.nextUrl));
+    }
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/profile',
+    '/login',
+    '/signup',
+    '/profile/:path*'
+  ],
+}
