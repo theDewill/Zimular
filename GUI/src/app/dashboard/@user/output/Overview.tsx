d7b799ca3ff3710e38a6d99e76178c37655565b4
navindu-sachintha <navindu.social@gmail.com>
2024-03-20 16:32:20 +0530

    En:Reconfigured chart components

--- GUI/src/app/dashboard/@user/output/Overview.tsx
+++ GUI/src/app/dashboard/@user/output/Overview.tsx
@@ -1,8 +1,8 @@
-import Areachart from "@/components/charts/Areachart";
+import Areachrt from "@/components/charts/Areachart";
 import Doughnutchart from "@/components/charts/Doughnutchart";
 import Horizontalchart from "@/components/charts/Horizontalchart";
-import Linechart from "@/components/charts/Linechart";
-import Multiaxislinechart from "@/components/charts/Multiaxislinechart";
+import LineBarAreachrt from "@/components/charts/LineBarAreachrt";
+import Linechrt from "@/components/charts/Linechart";
 import Piechart from "@/components/charts/Piechart";
 import Verticalchart from "@/components/charts/Verticalchart";
 import React from "react";
@@ -12,11 +12,11 @@ export default function Overview() {
         <div>
             {/* <Horizontalchart /> */}
             {/* <Verticalchart /> */}
-            {/* <Areachart /> */}
-            {/* <Linechart /> */}
-            {/* <Multiaxislinechart /> */}
+            {/* <Areachrt /> */}
+            {/* <Linechrt /> */}
+            <LineBarAreachrt />
             {/* <Piechart /> */}
-            <Doughnutchart/>
+            {/* <Doughnutchart/> */}
         </div>
     )
 }
--- GUI/src/components/charts/Linechart.tsx
+++ GUI/src/components/charts/Linechart.tsx
@@ -1,61 +1,84 @@
-import React from 'react'
+import React from "react";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-);
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 
-export const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top' as const,
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart',
-      },
-    },
-};
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+];
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const data = {
-    labels,
-    datasets:[
-        {
-            label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 55, -6],
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
-        {
-            label: 'My Second Dataset',
-            data: [28, 48, 40, 19, 86, 27, 55],
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-        }
-    ]
+export default function Linechrt() {
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="pv"
+        stroke="#8884d8"
+        activeDot={{ r: 8 }}
+      />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+    </LineChart>
+  );
 }
-
-
-const Linechart = () => {
-  return <Line data={data} options={options} />;
-}
-
-export default Linechart
