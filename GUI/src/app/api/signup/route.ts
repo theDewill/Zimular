eaa09c3b9d0b47c902a17ac259c0ec18577d4cdf
navindu-sachintha <navindu.social@gmail.com>
2024-03-22 15:28:26 +0530

    En:Updated component table

--- GUI/src/app/api/signup/route.ts
+++ GUI/src/app/api/signup/route.ts
@@ -1,29 +1,36 @@
 import User from "@/models/User";
 import connect from "@/utils/db";
 import bcrypt from "bcryptjs";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export const POST = async (request: any) => {
-  const { email, password } = await request.json();
+connect();
 
-  await connect();
+export const POST = async (request: NextRequest) => {
+  
 
-  const existingUser = await User.findOne({ email });
-
-  if (existingUser) {
-    return new NextResponse("Email is already in use", { status: 400 });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 5);
-  const newUser = new User({
-    email,
-    password: hashedPassword,
-  });
   try {
+    const { email, password } = await request.json();
+
+    //Check if the user Exist
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return new NextResponse("Email is already in use", { status: 400 });
+    }
+    
+    //Hash the password
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+    });
+
     await newUser.save();
-    return new NextResponse("user is registered", { status: 200 });
+    return new NextResponse("User is registered", { status: 200 });
+    
   } catch (err: any) {
-    return new NextResponse("Error Occured", {
+    return new NextResponse(err.message, {
       status: 500,
     });
   }
