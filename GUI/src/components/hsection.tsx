7598fde1d609a01f3039f5b9a97a7b506038991a
PasinduVithanage <pasindumadubhashitha99@gmail.com>
2024-03-24 01:51:58 +0530

    Change all

--- GUI/src/components/hsection.tsx
+++ GUI/src/components/hsection.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const NewSection = () => {
   return (
-    <section className="bg-gray-100 py-8 lg:py-12">
+    <section className="bg-gray-100 py-8 lg:py-12 p-[350px]">
       <div className="container mx-auto px-4">
         <h2 className="text-2xl lg:text-3xl font-bold text-center mb-4 lg:mb-6">What is Zimular</h2>
-        <p className="text-lg lg:text-xl text-gray-700 leading-relaxed mb-4 lg:mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit error placeat, nam aliquid rerum architecto porro odio magnam iusto nemo asperiores esse natus quaerat, magni nostrum! Ullam sapiente omnis aliquam? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, veniam debitis autem architecto reiciendis adipisci exercitationem ullam laudantium eaque officia repudiandae atque ad est similique! Voluptatum ex nulla sequi assumenda.l Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quibusdam in nobis beatae! Corporis, consectetur dolores porro error dolore ullam velit reiciendis! Neque, vero quo sequi doloremque cumque dicta voluptates.e</p>
+        <p className="text-lg lg:text-xl text-gray-700 leading-relaxed mb-4 lg:mb-8">we're dedicated to revolutionizing the way you manage your tasks and projects. With our groundbreaking workload simulation tool, predicting and optimizing task durations has never been easier. 
+
+          Imagine effortlessly ensuring a streamlined workflow, where every task is executed with precision and efficiency. Our tool empowers you to experience the future of productivity firsthand.
+
+          Gone are the days of guesswork and uncertainty. With our advanced simulation technology, you can start simulating your workload today and unlock unparalleled insights into your operations.
+
+          Discover a new era of productivity with Zimular. Join us as we redefine the possibilities of task management and propel your business towards unprecedented success.</p>
       </div>
     </section>
   );
